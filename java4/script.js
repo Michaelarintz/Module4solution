6206ed17c699b7241c0e87ef91de923eb47a1269
Carsten Rintz <carstenrintz@Carstens-iMac.Home>
2020-03-23 14:12:47 +0000

    js.

--- java4/script.js
+++ java4/script.js
@@ -27,10 +27,7 @@ assignment.
 WARNING!!! WARNING!!!
 */
 
-// STEP 1:
-// Wrap the entire contents of script.js inside of an IIFE
-// See Lecture 52, part 2
-// (Note, Step 2 will be done in the SpeakHello.js file.
+
 (function () {
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 for (var i = 0; i < names.length; i++) {
